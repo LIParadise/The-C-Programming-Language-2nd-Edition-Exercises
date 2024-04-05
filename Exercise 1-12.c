bc125e1192e6c3cc03dec67069d3dc103bc88f0f
LIParadise <a58524andy@gmail.com>
2024-04-05 23:35:11 +0800

    [1-12] probably working

--- Exercise 1-12.c
+++ Exercise 1-12.c
@@ -16,91 +16,51 @@ int main(int argc, char **argv) {
         print_help_and_exit();
     }
     FILE *input = fopen(argv[1], "r");
-    static const size_t BUF_LEN = 2;
-    bool word_span_across_lines = false;
-    bool skip_remaining_parts = false;
-    for (char buf[BUF_LEN]; fgets(buf, BUF_LEN, input) == buf;) {
-        size_t start, end, newline;
-        for (newline = 0; newline < BUF_LEN && buf[newline] != '\n'; ++newline)
-            ;
-        if (skip_remaining_parts) {
-            // A really long line spanning across multiple buffer refills,
-            // and we've printed the first word.
-            // Ignore all till newline is found
-            if (newline != BUF_LEN) {
-                skip_remaining_parts = false;
+    static const size_t BUF_LEN = 3;
+    char buf[BUF_LEN];
+    size_t start, end;
+    bool print_newline_upon_space = false, buf_has_newline = false,
+         line_has_word = false;
+    while (fgets(buf, BUF_LEN, input) == buf) {
+        buf_has_newline = false;
+        for (size_t i = 0; buf[i] != '\0'; ++i) {
+            if (buf[i] == '\n') {
+                buf_has_newline = true;
+                break;
+            }
+        }
+        start = 0;
+        if (print_newline_upon_space && isspace(buf[start])) {
+            putchar('\n');
+            print_newline_upon_space = false;
+        }
+        while (start < BUF_LEN) {
+            while (isspace(buf[start])) {
+                ++start;
+            }
+            end = start;
+            while (buf[end] != '\0' && !isspace(buf[end])) {
+                ++end;
+            }
+            if (end > start) {
+                line_has_word = true;
+                if (isspace(buf[end])) {
+                    buf[end] = '\0';
+                    printf("%s\n", buf + start);
+                    print_newline_upon_space = false;
+                } else if (buf[end] == '\0') {
+                    printf("%s", buf + start);
+                    print_newline_upon_space = true;
+                }
+            }
+            start = end + 1;
+        }
+        if (buf_has_newline) {
+            print_newline_upon_space = false;
+            if (!line_has_word) {
                 putchar('\n');
             }
-        } else {
-            // Probably have something to print...
-
-            if (!word_span_across_lines) {
-                // basic case
-                // search for word start and end, print if found
-                //
-                // Case A. the buf contains newline (line ends within buf)
-                // Case B. the buf contains no newline (line longer than buf)
-                //
-                // For case A., just print the word and newline.
-                //
-                // For case B., either it's space all the way down, in which
-                // case we print nothing, or there's some non space characters
-                // that form a word which may or may not extend over the next
-                // buffer refill
-
-                // '\0' is always appended at the end by fgets (man 3 fgets)
-                // '\0' is not a space (man 3 isspace)
-                // hence no boundary check required
-                for (start = 0; isspace(buf[start]); ++start)
-                    ;
-                for (end = start; buf[end] != '\0' && !isspace(buf[end]); ++end)
-                    ;
-
-                if (newline != BUF_LEN) {
-                    // case A
-                    buf[end] = 0;
-                    buf[newline] = 0;
-                    printf("%s\n", buf + start);
-                } else {
-                    // case B
-                    if (buf[start] == '\0') {
-                        // space all the way down
-                        // ignore this part and refill buffer
-                    } else {
-                        // a word to print
-                        // determine either the word ends in this buffer or not.
-                        // if word end within this buffer, skip remaining parts
-                        word_span_across_lines = buf[end] == '\0';
-                        skip_remaining_parts = buf[end] != '\0';
-                        buf[end] = '\0';
-                        printf("%s", buf + start);
-                    }
-                }
-            } else {
-                // first word span across buffers, word start already printed
-                //
-                // Case A. line doesn't end in this buffer
-                // Case B. line ends in this buffer
-                //
-                // Case A.a neither does the word end in the buffer
-                // Case A.b word ends but line continues
-                for (end = 0; buf[end] != '\0' && !isspace(buf[end]); ++end)
-                    ;
-                if (newline == BUF_LEN) {
-                    // Case A
-                    // notice Case A.a and Case A.b could be solved together
-                    word_span_across_lines = buf[end] == '\0';
-                    skip_remaining_parts = buf[end] != '\0';
-                    buf[end] = '\0';
-                    printf("%s", buf);
-                } else {
-                    // Case B.
-                    word_span_across_lines = false;
-                    buf[end] = 0;
-                    buf[newline] = 0;
-                    printf("%s\n", buf);
-                }
-            }
+            line_has_word = false;
         }
     }
     fclose(input);
